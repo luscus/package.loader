a402ad8ee6832ee8ae3c5eb5691058efbb865e62
luscus <luscus.redbeard@gmail.com>
2014-11-20 01:13:43 +0100

    added keywords

--- test/test.js
+++ test/test.js
@@ -1,38 +1,3 @@
 var plugin = require('../lib/package.loader');
 
 console.log(plugin);
-
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('dependencies:');
-console.log(plugin.dependencies);
-
-
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('Test require("package.root.finder")');
-
-console.log(plugin.require('package.root.finder'));
-
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('Test match(/^package.*/)');
-
-console.log(plugin.match(/^package.*/));
-
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('Test load(/^package.*/)');
-
-console.log(plugin.load(/^package.*/));
-
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('Test loadAll(/^package.*/)');
-
-console.log(plugin.loadAll(/^package.*/));
-
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('Test match(/^dadadadT.*/)');
-
-console.log(plugin.match(/^dadadadT.*/));
-
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('Test match(invalidRegexp)');
-
-console.log(plugin.match('invalidRegexp'));
